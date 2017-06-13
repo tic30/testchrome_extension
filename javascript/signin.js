5c87eea82db2d60576c7f92948670eca2d47a364
Tim Chu <chutianxin@gmail.com>
2017-06-13 01:36:37 -0400

    v0.1 test only

--- javascript/signin.js
+++ javascript/signin.js
@@ -1,6 +1,7 @@
 var jobDetail = "";
 var currenthostname = "";
-
+var mustHaveSkills = ["basic", "qualifications", "required", "experience", "expertise", "minimum"];
+var goodToHaveSkills = ["preferred", "qualifications", "ideal", "candidate"];//"is a plus"
 function getURLInfo() {
     var currentUrl;
     chrome.tabs.query({
@@ -49,49 +50,77 @@ function findKeyword(keyword) {
 }
 
 chrome.runtime.onMessage.addListener(function (request, sender) {
-    if (request.action === "getSource" && currenthostname === "linkedin.com") {
+    if (request.action === "getSource"){
         var pageSource = $.parseHTML(request.source);
-        jobDetail = $(pageSource).find("#job-details").prop('innerHTML').toLowerCase();
-        //console.log("pageSource fetch complete, get:");
-        //console.log(jobDetail);
-        //console.log(chrome.extension.getURL('/jsons/keywords.json'));
-        /*
-        var xhr = new XMLHttpRequest();
-        xhr.onreadystatechange = function () {
-            if (xhr.readyState === 4) {
-                var keywordArray = JSON.parse(xhr.responseText);
-                //var keywordArray2 = ["javascript", "php", "sass"];
-                console.log("keyword array is: " + keywordArray);
-                var filteredKey = keywordArray.filter(findKeyword);
+        var detailsDiv = "";
+        if(currenthostname === "linkedin.com") {
+            detailsDiv = $(pageSource).find("#job-details");
+            var easyApply = $(pageSource).find(".jobs-details-top-card__actions");
+            if(easyApply && easyApply.find('button[data-control-name="jobdetails_topcard_inapply"]').length > 0){
+                document.querySelector('#quickApply').innerText = "Easy Apply via LinkedIn !!!";
+            }
+        }else if (currenthostname === "glassdoor.com"){
+            //var activeHeaderWrapper = $(pageSource).find('#HeroHeaderModule');
+            detailsDiv = $(pageSource).find(".jobDescriptionContent");
+            //var easyApply = activeHeaderWrapper.find('div[class="tbl fill borderBot anchor navButtons"]');
+            //console.log(easyApply);
+            if($(pageSource).find('.footerActions a[href="#easy-apply-module"]').length > 0){
+               document.querySelector('#quickApply').innerText = "Easy Apply via Glassdoor !!!";
+            }
+        }else{
+            document.querySelector('#content-wrapper').innerText = "Currently only support LinkedIn and Glassdoor";
+            return;
+        }
+        if(detailsDiv.length > 0){
+            jobDetail = detailsDiv.prop('innerHTML');//.toLowerCase();
+            jobDetail = jobDetail.replace(/[^\w\s\s+<>]/g,' ');
+            console.log(jobDetail);
+            //console.log("pageSource fetch complete, get:");
+            //console.log(jobDetail);
+            //console.log(chrome.extension.getURL('/jsons/keywords.json'));
+            /*
+            var xhr = new XMLHttpRequest();
+            xhr.onreadystatechange = function () {
+                if (xhr.readyState === 4) {
+                    var keywordArray = JSON.parse(xhr.responseText);
+                    //var keywordArray2 = ["javascript", "php", "sass"];
+                    console.log("keyword array is: " + keywordArray);
+                    var filteredKey = keywordArray.filter(findKeyword);
+                    //debugger;
+                    console.log("filteredKey is: " + filteredKey);
+                    if (filteredKey == []) {
+                        message.innerText = "Sorry, found nothing";
+                        return;
+                    }
+                    message.innerText = "Hard Skills: " + filteredKey;
+                }
+            };
+            xhr.open("GET", chrome.extension.getURL('/jsons/keywords.json'), true);
+            xhr.send();
+            */
+            debugger;
+            //$.getJSON(chrome.extension.getURL('/jsons/keywords.json'), function(data) {
                 //debugger;
+                //console.log("data is: " + data);
+                //var keywordArray = JSON.parse(data);
+                //var keywordArray2 = ["javascript", "php", "sass", "html", "css", "ajax"];
+                //console.log("keyword array is: " + keywordArray);
+                var filteredKey = programLanguages.filter(findKeyword);
                 console.log("filteredKey is: " + filteredKey);
-                if (filteredKey == []) {
-                    message.innerText = "Sorry, found nothing";
+                var outputKeywords = '';
+                for(var keyword in filteredKey){
+                    if(filteredKey[keyword].length > 1) outputKeywords += filteredKey[keyword] + ', '; 
+                }
+                outputKeywords = outputKeywords.substring(0, outputKeywords.length - 2);
+                if (outputKeywords.length == 0) {
+                    document.querySelector('#message').innerText = "Mmm, nothing matches";
                     return;
                 }
-                message.innerText = "Hard Skills: " + filteredKey;
-            }
-        };
-        xhr.open("GET", chrome.extension.getURL('/jsons/keywords.json'), true);
-        xhr.send();
-        */
-
-        //$.getJSON(chrome.extension.getURL('/jsons/keywords.json'), function(data) {
-            //debugger;
-            //console.log("data is: " + data);
-            //var keywordArray = JSON.parse(data);
-            var keywordArray2 = ["javascript", "php", "sass", "html", "css", "ajax"];
-            console.log("keyword array is: " + keywordArray2);
-            var filteredKey = keywordArray2.filter(findKeyword);
-            console.log("filteredKey is: " + filteredKey);
-            if (filteredKey.length == 0) {
-                message.innerText = "Mmm, nothing matches";
-                return;
-            }
-            message.innerText = "Hard Skills: " + filteredKey;
-        //});
-    } else {
-        message.innerText = "Currently only support LinkedIn";
+                document.querySelector('#message').innerText = outputKeywords;
+            //});
+        } else {
+            document.querySelector('#content-wrapper').innerText = "Please go to a job posting page";
+        }
     }
 });
 
@@ -112,8 +141,10 @@ function scanPage() {
 
 $(document).ready(function () {
     getURLInfo();
-
+    scanPage();
+    /*
     $('.btn').click(function (e) {
         scanPage();
-    })
+    })*/
+    
 });
